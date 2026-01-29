3aeb842df247ddb4951e6677ceaa89a1c54efa6e
HumanAI Convention <ben@humanaiconvention.com>
2026-01-29 10:05:08 -0800

    Deploy: UI Recovery + GCS Fix

---
+++ web/dist/assets/analytics-engagement-DgI1MVx4.js
@@ -0,0 +1,1 @@
+import{a as e,i as r,s as a}from"./analytics-core-Bh-KxoFz.js";import"./analytics-errors-CMEIYE25.js";export{e as initSectionObserver,r as initVisibilityListeners,a as startHeartbeat};
--- web/dist/assets/analytics-engagement-M51JQkpr.js
+++
@@ -1,1 +0,0 @@
-import{a as e,i as r,s as a}from"./analytics-core-BhNu_vsJ.js";import"./analytics-errors-CC2HdOEk.js";export{e as initSectionObserver,r as initVisibilityListeners,a as startHeartbeat};
